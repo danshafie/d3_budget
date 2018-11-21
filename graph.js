07231f832803ee000af878ba2375716edac04757
danshafie <danshafie@gmail.com>
2018-11-20 18:32:21 -0800

    added legend

--- graph.js
+++ graph.js
@@ -1,6 +1,133 @@
+// const dims = { height: 300, width: 300, radius: 150 };
+// const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
+
+// const svg = d3
+//   .select(".canvas")
+//   .append("svg")
+//   .attr("width", dims.width + 150)
+//   .attr("height", dims.height + 150);
+
+// const graph = svg
+//   .append("g")
+//   .attr("transform", `translate(${cent.x}, ${cent.y})`);
+
+// const pie = d3
+//   .pie()
+//   .sort(null)
+//   .value(d => d.cost);
+
+// const arcPath = d3
+//   .arc()
+//   .outerRadius(dims.radius)
+//   .innerRadius(dims.radius / 2);
+
+// //color scheme for pie sections
+// const colors = d3.scaleOrdinal(d3["schemeSet3"]);
+
+// //legend
+// const legendGroup = svg
+//   .append("g")
+//   .attr("transform", `translate(${dims.width + 40}, 10)`);
+
+// const legend = d3
+//   .legendColor()
+//   .shape("path", d3.symbol().type(d3.symbolCircle)())
+//   .shapePadding(10)
+//   .scale(colors);
+
+// const update = data => {
+//   //join data to elements
+//   const paths = graph.selectAll("path").data(pie(data));
+
+//   legendGroup.call(legend);
+
+//   //map colors scheme to names
+//   colors.domain(data.map(d => d.name));
+
+//   //remove exit selection
+//   paths
+//     .exit()
+//     .transition()
+//     .duration(750)
+//     .attrTween("d", arcTweenExit)
+//     .remove();
+
+//   //update current shapes
+//   paths
+//     .transition()
+//     .duration(750)
+//     .attrTween("d", arcTweenUpdate);
+
+//   //append new shapes
+//   paths
+//     .enter()
+//     .append("path")
+//     .attr("class", "arc")
+//     // .attr("d", arcPath)
+//     .attr("stroke", "#fff")
+//     .attr("stroke-width", 3)
+//     .attr("fill", d => colors(d.data.name))
+//     .transition()
+//     .duration(750)
+//     .attrTween("d", arcTweenEnter);
+// };
+
+// let data = [];
+
+// db.collection("expenses").onSnapshot(res => {
+//   res.docChanges().forEach(change => {
+//     // console.log("change: ", change);
+//     const doc = { ...change.doc.data(), id: change.doc.id };
+//     if (change.type === "added") {
+//       data.push(doc);
+//     } else if (change.type === "modified") {
+//       const index = data.findIndex(item => item.id === doc.id);
+//       console.log("index: ", index);
+//       data[index] = doc;
+//     } else if (change.type === "removed") {
+//       data = data.filter(item => item.id !== doc.id);
+//     }
+//   });
+//   //   console.log("data: ", data);
+
+//   update(data);
+// });
+
+// const arcTweenEnter = d => {
+//   let i = d3.interpolate(d.endAngle, d.startAngle);
+
+//   return function(t) {
+//     d.startAngle = i(t);
+//     return arcPath(d);
+//   };
+// };
+
+// const arcTweenExit = d => {
+//   let i = d3.interpolate(d.startAngle, d.endAngle);
+
+//   return function(t) {
+//     d.startAngle = i(t);
+//     return arcPath(d);
+//   };
+// };
+
+// // use function keyword to allow use of 'this'
+// function arcTweenUpdate(d) {
+//   console.log(this._current, d);
+//   // interpolate between the two objects
+//   var i = d3.interpolate(this._current, d);
+//   // update the current prop with new updated data
+//   this._current = i(1);
+
+//   return function(t) {
+//     // i(t) returns a value of d (data object) which we pass to arcPath
+//     return arcPath(i(t));
+//   };
+// }
 const dims = { height: 300, width: 300, radius: 150 };
 const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
 
+// create svg container
 const svg = d3
   .select(".canvas")
   .append("svg")
@@ -10,28 +137,46 @@ const svg = d3
 const graph = svg
   .append("g")
   .attr("transform", `translate(${cent.x}, ${cent.y})`);
+// translates the graph group to the middle of the svg container
 
 const pie = d3
   .pie()
   .sort(null)
   .value(d => d.cost);
+// the value we are evaluating to create the pie angles
 
 const arcPath = d3
   .arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-//color scheme for pie sections
-const colors = d3.scaleOrdinal(d3["schemeSet3"]);
+// ordinal colour scale
+const colour = d3.scaleOrdinal(d3["schemeSet3"]);
 
+// legend setup
+const legendGroup = svg
+  .append("g")
+  .attr("transform", `translate(${dims.width + 40}, 10)`);
+
+const legend = d3
+  .legendColor()
+  .shape("path", d3.symbol().type(d3.symbolCircle)())
+  .shapePadding(10)
+  .scale(colour);
+
+// update function
 const update = data => {
-  //join data to elements
+  // update colour scale domain
+  colour.domain(data.map(d => d.name));
+
+  // update legend
+  legendGroup.call(legend);
+  legendGroup.selectAll("text").attr("fill", "white");
+
+  // join enhanced (pie) data to path elements
   const paths = graph.selectAll("path").data(pie(data));
 
-  //map colors scheme to names
-  colors.domain(data.map(d => d.name));
-
-  //remove exit selection
+  // handle the exit selection
   paths
     .exit()
     .transition()
@@ -39,49 +184,59 @@ const update = data => {
     .attrTween("d", arcTweenExit)
     .remove();
 
-  //update current shapes
+  // handle the current DOM path updates
   paths
     .transition()
     .duration(750)
     .attrTween("d", arcTweenUpdate);
 
-  //append new shapes
   paths
     .enter()
     .append("path")
     .attr("class", "arc")
-    // .attr("d", arcPath)
     .attr("stroke", "#fff")
     .attr("stroke-width", 3)
-    .attr("fill", d => colors(d.data.name))
+    .attr("d", arcPath)
+    .attr("fill", d => colour(d.data.name))
+    .each(function(d) {
+      this._current = d;
+    })
     .transition()
     .duration(750)
     .attrTween("d", arcTweenEnter);
 };
 
-let data = [];
+// data array and firestore
+var data = [];
 
-db.collection("expenses").onSnapshot(res => {
-  res.docChanges().forEach(change => {
-    // console.log("change: ", change);
-    const doc = { ...change.doc.data(), id: change.doc.id };
-    if (change.type === "added") {
-      data.push(doc);
-    } else if (change.type === "modified") {
-      const index = data.findIndex(item => item.id === doc.id);
-      console.log("index: ", index);
-      data[index] = doc;
-    } else if (change.type === "removed") {
-      data = data.filter(item => item.id !== doc.id);
-    }
+db.collection("expenses")
+  .orderBy("cost")
+  .onSnapshot(res => {
+    res.docChanges().forEach(change => {
+      const doc = { ...change.doc.data(), id: change.doc.id };
+
+      switch (change.type) {
+        case "added":
+          data.push(doc);
+          break;
+        case "modified":
+          const index = data.findIndex(item => item.id == doc.id);
+          data[index] = doc;
+          break;
+        case "removed":
+          data = data.filter(item => item.id !== doc.id);
+          break;
+        default:
+          break;
+      }
+    });
+
+    // call the update function
+    update(data);
   });
-  //   console.log("data: ", data);
-
-  update(data);
-});
 
 const arcTweenEnter = d => {
-  let i = d3.interpolate(d.endAngle, d.startAngle);
+  var i = d3.interpolate(d.endAngle - 0.1, d.startAngle);
 
   return function(t) {
     d.startAngle = i(t);
@@ -90,7 +245,7 @@ const arcTweenEnter = d => {
 };
 
 const arcTweenExit = d => {
-  let i = d3.interpolate(d.startAngle, d.endAngle);
+  var i = d3.interpolate(d.startAngle, d.endAngle);
 
   return function(t) {
     d.startAngle = i(t);
