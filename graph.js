8a26b736f6a59c3e3faea74900797b537eedbfb2
danshafie <danshafie@gmail.com>
2018-11-20 17:54:02 -0800

    added exit and update to update function

--- graph.js
+++ graph.js
@@ -2,7 +2,7 @@ const dims = { height: 300, width: 300, radius: 150 };
 const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
 
 const svg = d3
-  .select("canvas")
+  .select(".canvas")
   .append("svg")
   .attr("width", dims.width + 150)
   .attr("height", dims.height + 150);
@@ -16,17 +16,48 @@ const pie = d3
   .sort(null)
   .value(d => d.cost);
 
-const arcPath = d3.arc().outerRadius(dims.radius);
+const arcPath = d3
+  .arc()
+  .outerRadius(dims.radius)
+  .innerRadius(dims.radius / 2);
+
+//color scheme for pie sections
+const colors = d3.scaleOrdinal(d3["schemeSet3"]);
 
 const update = data => {
-  console.log("data: ", data);
+  //join data to elements
+  const paths = graph.selectAll("path").data(pie(data));
+
+  //map colors scheme to names
+  colors.domain(data.map(d => d.name));
+
+  //remove exit selection
+  paths.exit().remove();
+
+  //update current shapes
+  paths
+    // .attr("class", "arc")
+    .attr("d", arcPath)
+    // .attr("stroke", "#fff")
+    // .attr("stroke-width", 3)
+    .attr("fill", d => colors(d.data.name));
+
+  //append new shapes
+  paths
+    .enter()
+    .append("path")
+    .attr("class", "arc")
+    .attr("d", arcPath)
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 3)
+    .attr("fill", d => colors(d.data.name));
 };
 
 let data = [];
 
 db.collection("expenses").onSnapshot(res => {
   res.docChanges().forEach(change => {
-    console.log("change: ", change);
+    // console.log("change: ", change);
     const doc = { ...change.doc.data(), id: change.doc.id };
     if (change.type === "added") {
       data.push(doc);
@@ -38,6 +69,7 @@ db.collection("expenses").onSnapshot(res => {
       data = data.filter(item => item.id !== doc.id);
     }
   });
+  //   console.log("data: ", data);
 
   update(data);
 });
